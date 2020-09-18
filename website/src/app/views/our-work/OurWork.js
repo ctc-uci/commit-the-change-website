b68e9c1c7f1f9f207ea217f6cf7c2a543f09c035
nguyenethan01 <nguyenethan01@gmail.com>
2020-09-17 20:16:31 -0700

    our-work-background

--- website/src/app/views/our-work/OurWork.js
+++ website/src/app/views/our-work/OurWork.js
@@ -3,7 +3,6 @@ import './OurWork.css';
 import ProjectCard from '../../components/ProjectCard/ProjectCard';
 import ProjectCardPurple from '../../components/ProjectCardPurple/ProjectCardPurple';
 import ourWorkPic from '../../../images/our-work.svg';
-import scroll from '../../../images/scroll.svg';
 import crimeSurvivors from '../../../images/our-work-crime-survivors.svg';
 import * as projectData from './ProjectCard.json';
 
@@ -18,16 +17,13 @@ function OurWork() {
         </div>
         <img src={ourWorkPic} alt="our-work" className="our-work-img" />
       </div>
-      <div id="middle-our-work">
+      <div id="middle-our-work" b>
         <h1 className="upcoming-projects">Our Upcoming Projects</h1>
         <div className="projects">
           <ProjectCard className="projectcard" imageURL={projectData.project1.imageURL} projectName={projectData.project1.projectName} description={projectData.project1.description} projectURL={projectData.project1.projectURL} />
           <ProjectCard className="projectcard" imageURL={projectData.project2.imageURL} projectName={projectData.project2.projectName} description={projectData.project2.description} projectURL={projectData.project2.projectURL} />
           <ProjectCard className="projectcard" imageURL={projectData.project3.imageURL} projectName={projectData.project3.projectName} description={projectData.project3.description} projectURL={projectData.project3.projectURL} />
         </div>
-        <a href="#bottom-our-work" className="scroll-button-element">
-          <img src={scroll} alt="scroll-button" />
-        </a>
       </div>
       <div id="bottom-our-work">
         <div className="bottom-card">
