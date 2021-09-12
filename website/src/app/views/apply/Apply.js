1a1561a413418f7f6039af40101b462dd25c066e
Zubair Sidhu <sidhuzubair@gmail.com>
2021-09-11 23:17:32 -0700

    Added first section of apply page

--- website/src/app/views/apply/Apply.js
+++ website/src/app/views/apply/Apply.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import { animated, useSpring } from 'react-spring';
+import applyGraphic from '../../../images/apply/apply.svg';
+import animationConfig from '../animationConstants';
 import './Apply.css';
 
 function Apply() {
+  const slideUp = useSpring(animationConfig.slideUp(true));
   return (
     <main>
-      {/* Content here */}
+      <animated.div style={slideUp} className="top-panel">
+        <div className="top-panel-text">
+          <div className="inside-top-panel-text">
+            <h1 className="top-panel-title">Why join CTC?</h1>
+            <p className="top-panel-description">
+              Commit the Change is devoted to contributing to the community
+              by creating software products for non-profits. As part of this,
+              our developers and designers learn how to work both within their
+              teams and across cross-functional teams in an Agile-like environment.
+              Applying to CTC is a great opportunity to be part of an organization
+              that values innovative technology being used for social good!
+            </p>
+            <a href="#bottom" className="common-pink-button">
+              Apply
+            </a>
+          </div>
+        </div>
+        <div className="top-panel-pic">
+          <img
+            src={applyGraphic}
+            className="apply-top-panel-img"
+            alt="Apply to Commit the Change!"
+          />
+        </div>
+      </animated.div>
     </main>
   );
 }
