9a4096b2907d830e62dc4ab004995f61ab3de555
Zubair Sidhu <sidhuzubair@gmail.com>
2021-09-14 22:59:20 -0700

    Add all timeline values

--- website/src/app/views/apply/timelineValues.js
+++ website/src/app/views/apply/timelineValues.js
@@ -9,7 +9,19 @@ const timelineValues = [
     leftText: 'Week 1',
     header: 'Applications Due',
     date: 'Oct 1',
-    description: ' Our application consists of a few short response questions so we can learn more about your experience, your goals, and your background. Applications open September 19th and will close October 1st.',
+    description: 'Our application consists of a few short response questions so we can learn more about your experience, your goals, and your background. Applications open September 19th and will close October 1st.',
+  },
+  {
+    leftText: 'Week 2',
+    header: 'Coffee Chats',
+    date: 'Oct 4 - Oct 8',
+    description: 'Coffee chats are one-on-one’s with a board member by invitation. But no pressure - this is a casual way for us to get to know you better and for you to learn more about Commit the Change!',
+  },
+  {
+    leftText: 'Week 3',
+    header: 'Decision',
+    date: 'Oct 10',
+    description: 'Check your emails on October 10th to see your application status. Regardless of the outcome, we truly appreciate you taking the time to see what Commit the Change has to offer! Thanks for sticking with us these past few weeks!',
   },
 ];
 
