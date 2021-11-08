ffd661f9f8e12518b266e308acc50de02b056b96
kanuc1 <kanu.chandra95@gmail.com>
2021-11-08 00:30:53 -0800

    made changes to resolve the conflict

--- website/src/app/views/about/members.js
+++ website/src/app/views/about/members.js
@@ -131,7 +131,6 @@ import evan from '../../../images/about/memberImages/evan.JPG';
 import hana from '../../../images/about/memberImages/hanalee.jpg';
 import kavan from '../../../images/about/memberImages/kavan.jpeg';
 import caroline from '../../../images/about/memberImages/caroline.jpeg';
-import kanu from '../../../images/about/memberImages/kanu.jpeg';
 
 const members = {
   info: [
@@ -500,12 +499,6 @@ const members = {
       position: 'Developer',
       linkedinURL: 'https://www.linkedin.com/in/joshua-montellano-b753391a3/',
       imageURL: joshuamontellano,
-    },
-    {
-      name: 'Kanu Chandra',
-      position: 'Developer',
-      linkedinURL: 'https://www.linkedin.com/in/kanu-chandra-30310916b/',
-      imageURL: kanu,
     },
     {
       name: 'Kasey Chuang',
