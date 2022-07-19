83e21a6a6b3f941a189574b58b6d66c2d0187f04
Alan Chang <alan@alanchang.xyz>
2022-07-19 14:06:04 -0700

    Add first iteration of new navbar, change Home/About button styles

--- website/src/app/components/Navbar/Nav.js
+++ website/src/app/components/Navbar/Nav.js
@@ -19,120 +19,142 @@ function Nav() {
   const slideDownMobileNav = useSpring(animationConfigs.slideDownNav(open));
 
   return (
-    <nav>
-      <div>
-        <animated.div
-          style={slideDownMobileNav}
-          className="mobile-nav-links-container"
-        >
-          <div className="mobile-nav-content">
-            <div className="mobile-nav-links">
-              <Link
-                onClick={() => {
-                  toggleVisibility(false);
-                }}
-                to="/"
-              >
-                Home
-              </Link>
-              <Link
-                onClick={() => {
-                  toggleVisibility(false);
-                }}
-                to="/about"
-              >
-                About
-              </Link>
-              <Link
-                onClick={() => {
-                  toggleVisibility(false);
-                }}
-                to="/projects"
-              >
-                Our Work
-              </Link>
-              <Link
-                onClick={() => {
-                  toggleVisibility(false);
-                }}
-                to="/contact"
-              >
-                Contact Us
-              </Link>
-              {/* <Link
-                onClick={() => {
-                  toggleVisibility(false);
-                }}
-                to="/apply"
-              >
-                Apply Now
-              </Link> */}
-            </div>
-            <button
-              type="button"
-              className="mobile-nav"
-              onClick={() => {
-                toggleVisibility(!open);
-              }}
-              onKeyDown={() => {
-                toggleVisibility(!open);
-              }}
-            >
-              <img
-                className="mobile-nav-close-icon"
-                src={closeIcon}
-                alt="Close Button"
-              />
-            </button>
-          </div>
-        </animated.div>
-        <animated.div style={slideDown} className="nav-bar">
-          <div>
-            <Link to="/">
-              <img className="logo" src={ctcLogo} alt="Commit the Change logo" />
-            </Link>
-          </div>
-          <ul className="navigation">
-            <li>
-              <Link to="/about" className="elem">
-                About
-              </Link>
-            </li>
-            <li>
-              <Link to="/projects" className="elem">
-                Our Work
-              </Link>
-            </li>
-            <li>
-              <button type="button" className="elem apply-button">
-                <Link to="/contact" id="apply-link">
-                  Contact Us
-                </Link>
-              </button>
-            </li>
-            {/* <li>
-              <button type="button" className="elem apply-button">
-                <Link to="/apply" id="apply-link">
-                  Apply
-                </Link>
-              </button>
-            </li> */}
-          </ul>
-          <button
-            type="button"
-            className="mobile-nav"
-            onClick={() => {
-              toggleVisibility(!open);
-            }}
-            onKeyDown={() => {
-              toggleVisibility(!open);
-            }}
-          >
-            <i className="fa fa-3x fa-bars hamburger" />
-          </button>
-        </animated.div>
+    <nav className="nav-bar">
+      <Link to="/">
+        <div className="navbar-logo" />
+      </Link>
+      <div className="navbar-links">
+        <Link className="navbar-link" to="/about">About Us</Link>
+        <Link className="navbar-link" to="/projects">Projects</Link>
+        <Link className="navbar-link" to="/our-team">Our Team</Link>
+        <Link className="navbar-link" to="/contact">Contact</Link>
+        <Link className="navbar-link" to="/apply">Apply</Link>
       </div>
+      {/* <animated.div
+        style={slideDownMobileNav}
+        className="mobile-nav-links-container"
+      >
+        <div className="mobile-nav-content">
+          <div className="mobile-nav-links">
+            <Link to="/"><img src="../../../images/logo/purple-ctc.svg" /></Link>
+          </div>
+        </div>
+      </animated.div> */}
     </nav>
+    // <nav>
+    //   <div>
+    //     <animated.div
+    //       style={slideDownMobileNav}
+    //       className="mobile-nav-links-container"
+    //     >
+    //       <div className="mobile-nav-content">
+    //         <div className="mobile-nav-links">
+    //           <Link
+    //             onClick={() => {
+    //               toggleVisibility(false);
+    //             }}
+    //             to="/"
+    //           >
+    //             Home
+    //           </Link>
+    //           <Link
+    //             onClick={() => {
+    //               toggleVisibility(false);
+    //             }}
+    //             to="/about"
+    //           >
+    //             About
+    //           </Link>
+    //           <Link
+    //             onClick={() => {
+    //               toggleVisibility(false);
+    //             }}
+    //             to="/projects"
+    //           >
+    //             Our Work
+    //           </Link>
+    //           <Link
+    //             onClick={() => {
+    //               toggleVisibility(false);
+    //             }}
+    //             to="/contact"
+    //           >
+    //             Contact Us
+    //           </Link>
+    //           {/* <Link
+    //             onClick={() => {
+    //               toggleVisibility(false);
+    //             }}
+    //             to="/apply"
+    //           >
+    //             Apply Now
+    //           </Link> */}
+    //         </div>
+    //         <button
+    //           type="button"
+    //           className="mobile-nav"
+    //           onClick={() => {
+    //             toggleVisibility(!open);
+    //           }}
+    //           onKeyDown={() => {
+    //             toggleVisibility(!open);
+    //           }}
+    //         >
+    //           <img
+    //             className="mobile-nav-close-icon"
+    //             src={closeIcon}
+    //             alt="Close Button"
+    //           />
+    //         </button>
+    //       </div>
+    //     </animated.div>
+    //     <animated.div style={slideDown} className="nav-bar">
+    //       <div>
+    //         <Link to="/">
+    //           <img className="logo" src={ctcLogo} alt="Commit the Change logo" />
+    //         </Link>
+    //       </div>
+    //       <ul className="navigation">
+    //         <li>
+    //           <Link to="/about" className="elem">
+    //             About
+    //           </Link>
+    //         </li>
+    //         <li>
+    //           <Link to="/projects" className="elem">
+    //             Our Work
+    //           </Link>
+    //         </li>
+    //         <li>
+    //           <button type="button" className="elem apply-button">
+    //             <Link to="/contact" id="apply-link">
+    //               Contact Us
+    //             </Link>
+    //           </button>
+    //         </li>
+    //         {/* <li>
+    //           <button type="button" className="elem apply-button">
+    //             <Link to="/apply" id="apply-link">
+    //               Apply
+    //             </Link>
+    //           </button>
+    //         </li> */}
+    //       </ul>
+    //       <button
+    //         type="button"
+    //         className="mobile-nav"
+    //         onClick={() => {
+    //           toggleVisibility(!open);
+    //         }}
+    //         onKeyDown={() => {
+    //           toggleVisibility(!open);
+    //         }}
+    //       >
+    //         <i className="fa fa-3x fa-bars hamburger" />
+    //       </button>
+    //     </animated.div>
+    //   </div>
+    // </nav>
   );
 }
 
