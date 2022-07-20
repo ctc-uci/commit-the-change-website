64e714ff020599aa47be6dddb83b18734bd2e06d
Alan Chang <alan@alanchang.xyz>
2022-07-19 18:12:37 -0700

    Add framer-motion and mess around on homepage tab 1

--- website/src/app/views/home/Home.js
+++ website/src/app/views/home/Home.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import './Home.css';
 import ReactPageScroller from 'react-page-scroller';
+import { AnimatePresence, motion } from 'framer-motion/dist/framer-motion';
 import { animated, useSpring, useTrail } from 'react-spring';
 import VisibilitySensor from 'react-visibility-sensor';
+
 import animationConfig from '../animationConstants';
 import Timeline from '../../components/Timeline/Timeline';
 import homeLandingPic from '../../../images/home/home-top-panel.svg';
@@ -81,9 +83,52 @@ function Home() {
         pageOnChange={handlePageChange}
         customPageNumber={currentPage}
       >
-        <div className="ctc-home-1-bg">
+        <motion.div
+          className="ctc-home-1-bg"
+          initial={{width: 0, height: 0}}
+          animate={{width: "100%", height: "100%"}}
+          transition={{duration: 1}}
+        >
           <div className="ctc-home-1-container">
-            <animated.div className="ctc-home-1-left" style={slideUp}>
+            <div className="ctc-home-1-left" style={slideUp}>
+              <motion.p
+                className="ctc-home-1-header"
+                initial={{height: 0, opacity: 0}}
+                animate={{height: "auto", opacity: 1}}
+                transition={{delay: 0.5, duration: 0.75}}
+              >
+                Commit the Change
+              </motion.p>
+              <motion.p
+                className="ctc-home-1-subhead"
+                initial={{height: 0, opacity: 0}}
+                animate={{height: "auto", opacity: 1}}
+                transition={{delay: 0.65, duration: 0.75}}
+              >
+                Tech with Purpose
+              </motion.p>
+              <motion.p
+                className="ctc-home-1-desc"
+                initial={{height: 0, opacity: 0}}
+                animate={{height: "auto", opacity: 1}}
+                transition={{delay: 0.8, duration: 0.75}}
+              >
+                We help non-profit organizations maximize their social impact
+                with better tools and technologies, by giving opportunities to UC Irvine
+                students to design and develop high-quality software projects.
+              </motion.p>
+              <a onClick={() => handlePageChange(1)} className="no-text-decoration">
+                <motion.div
+                  className="ctc-home-1-button"
+                  initial={{height: 0, opacity: 0}}
+                  animate={{height: "auto", opacity: 1}}
+                  transition={{delay: 1, duration: 0.5}}
+                >
+                  Learn More
+                </motion.div>  
+              </a>
+            </div>
+            {/* <animated.div className="ctc-home-1-left" style={slideUp}>
               <p className="ctc-home-1-header">Commit the Change</p>
               <p className="ctc-home-1-subhead">Tech with Purpose</p>
               <p className="ctc-home-1-desc">
@@ -94,12 +139,26 @@ function Home() {
                   Learn More
                 </div>  
               </a>
-            </animated.div>
-            <animated.div className="ctc-home-1-right" style={slideInLeft}>
+            </animated.div> */}
+            {/* <animated.div className="ctc-home-1-right" style={slideInLeft}>
               <img className="ctc-home-1-shadowed-logo" src={ctcShadowedLogo} />
-            </animated.div>
+            </animated.div> */}
+            <div className="ctc-home-1-right">
+              <motion.img
+                className="ctc-home-1-shadowed-logo"
+                src={ctcShadowedLogo}
+                initial={{opacity: 0, x: -100, y: -20}}
+                animate={{opacity: 1, x: 0, y: 0}}
+                transition={{delay: 0.75, duration: 1, type: "spring", bounce: 0.4}}
+                drag
+                dragSnapToOrigin
+                dragConstraints={{left: 50, right: -100, top: 100, bottom: 80}}
+                dragElastic={0.45}
+                dragTransition={{bounceStiffness: 200, bounceDamping: 10}}
+              />
+            </div>
           </div>
-        </div>
+        </motion.div>
         {/* Placeholder pages go here */}
         <div className="ctc-home-1-bg" />
         <div className="ctc-home-1-bg" />
