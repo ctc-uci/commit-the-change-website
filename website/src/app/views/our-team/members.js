b4c2131dfc386b03e5fe5df532cdd481640b6ec7
Alan Chang <alan@alanchang.xyz>
2022-07-21 22:47:11 -0700

    Fix Zoya title

--- website/src/app/views/our-team/members.js
+++ website/src/app/views/our-team/members.js
@@ -23,158 +23,158 @@ import madhusudhan from '../../../images/about/memberImages/madhusudhan.jpg';
 import carlos from '../../../images/about/memberImages/carlos.jpg';
 
 const members = {
-    board: {
-        name: "Board",
-        members: [
-            {
-                name: 'Jane Vo',
-                position: 'President',
-                linkedinURL: 'https://www.linkedin.com/in/janehv/',
-                imageURL: jane,
-            },
-            {
-                name: 'Maxim Alayev',
-                position: 'Internal Vice President',
-                linkedinURL: 'https://www.linkedin.com/in/maximalayev/',
-                imageURL: maxim,
-            },
-            {
-                name: 'Meghna Kaligotla',
-                position: 'External Vice President',
-                linkedinURL: 'https://www.linkedin.com/in/meghna-kaligotla-605/',
-                imageURL: meghna,
-            },
-            {
-                name: 'Alan Chang',
-                position: 'Co-Director of Technical',
-                linkedinURL: 'https://www.linkedin.com/in/acxyz/',
-                imageURL: alan,
-            },
-            {
-                name: 'Claude Yan',
-                position: 'Co-Director of Technical',
-                linkedinURL: 'https://www.linkedin.com/in/claude-yan-b41a3216a/',
-                imageURL: claude,
-            },
-            {
-                name: 'Sabrina Nguyen-Hoang',
-                position: 'Co-Director of Design',
-                linkedinURL: 'https://www.linkedin.com/in/sabrina-nguyen-hoang/',
-                imageURL: brie,
-            },
-            {
-                name: 'Alexandria Wang',
-                position: 'Co-Director of Design',
-                linkedinURL: 'https://www.linkedin.com/in/alexandria-wang-44066a187/',
-                imageURL: alexandria,
-            },
-            {
-                name: 'Justine Chou',
-                position: 'Director of Marketing',
-                linkedinURL: 'https://www.linkedin.com/in/justine-chou/',
-                imageURL: justine,
-            },
-            {
-                name: 'Mingjia Wang',
-                position: 'Co-Director of Education',
-                linkedinURL: 'https://www.linkedin.com/in/mingjia-wang/',
-                imageURL: mingjia,
-            },
-            {
-                name: 'David Lu',
-                position: 'Co-Director of Education',
-                linkedinURL: 'https://www.linkedin.com/in/david-lin-yi-lu/',
-                imageURL: david,
-            },
-            {  
-              name: 'Matthew Wu',
-              position: 'Treasurer',
-              linkedinURL: 'https://www.linkedin.com/in/matthew-caleb-wu/',
-              imageURL: matthew,
-            },
-            {
-                name: 'Michelle Lin',
-                position: 'Director of Membership',
-                linkedinURL: 'https://www.linkedin.com/in/michellelinCA',
-                imageURL: michellelin,
-            },
-            {
-                name: 'Noah Arsala',
-                position: 'Director of Outreach',
-                linkedinURL: 'https://www.linkedin.com/in/noah-arsala-0324911b3/',
-                imageURL: noah,
-            },
-        ],
-    },
-    npo1: {
-        name: "NPO 1",
-        members: [
-            {
-                name: 'Hang Cao',
-                position: 'Tech Lead',
-                linkedinURL: 'https://www.linkedin.com/in/hang-m-cao/',
-                imageURL: hang,
-            },
-            {
-                name: 'Justin Liao',
-                position: 'Tech Lead',
-                linkedinURL: 'https://www.linkedin.com/in/justin-liao-1949b3217/',
-                imageURL: justin,
-            },
-            {
-                name: 'Zoya Soy',
-                position: 'Designer',
-                linkedinURL: 'https://www.linkedin.com/in/zoyasoy/',
-                imageURL: zoya,
-            },
-        ],
-    },
-    npo2: {
-        "name": "NPO 2",
-        "members": [
-            {
-                name: 'Jenny Lee',
-                position: 'Tech Lead',
-                linkedinURL: 'https://www.linkedin.com/in/jenny-lee-6630201b2',
-                imageURL: jenny,
-            },
-            {
-                name: 'Ashley Liu',
-                position: 'Tech Lead',
-                linkedinURL: 'https://www.linkedin.com/in/ashley-liu-6314731b9/',
-                imageURL: ashley,
-            },
-            {
-                name: 'Allison Liu',
-                position: 'Design Lead',
-                linkedinURL: 'https://www.linkedin.com/in/hiallisonliu/',
-                imageURL: allisonliu,
-            },
-        ],
-    },
-    npo3: {
-        "name": "NPO 3",
-        "members": [
-            {
-              name: 'Gurneet Cheema',
-              position: 'Tech Lead',
-              linkedinURL: 'https://www.linkedin.com/in/jeff-cheema/',
-              imageURL: gurneetcheema,
-            },
-            {
-                name: 'Madhusudhan Sharma',
-                position: 'Tech Lead',
-                linkedinURL: 'https://www.linkedin.com/in/madhuxsharma/',
-                imageURL: madhusudhan,
-            },
-            {
-                name: 'Carlos Lim',
-                position: 'Design Lead',
-                linkedinURL: 'https://www.linkedin.com/in/carlosalim7/',
-                imageURL: carlos,
-            },
-        ],
-    }
+  board: {
+    name: 'Board',
+    members: [
+      {
+        name: 'Jane Vo',
+        position: 'President',
+        linkedinURL: 'https://www.linkedin.com/in/janehv/',
+        imageURL: jane,
+      },
+      {
+        name: 'Maxim Alayev',
+        position: 'Internal Vice President',
+        linkedinURL: 'https://www.linkedin.com/in/maximalayev/',
+        imageURL: maxim,
+      },
+      {
+        name: 'Meghna Kaligotla',
+        position: 'External Vice President',
+        linkedinURL: 'https://www.linkedin.com/in/meghna-kaligotla-605/',
+        imageURL: meghna,
+      },
+      {
+        name: 'Alan Chang',
+        position: 'Co-Director of Technical',
+        linkedinURL: 'https://www.linkedin.com/in/acxyz/',
+        imageURL: alan,
+      },
+      {
+        name: 'Claude Yan',
+        position: 'Co-Director of Technical',
+        linkedinURL: 'https://www.linkedin.com/in/claude-yan-b41a3216a/',
+        imageURL: claude,
+      },
+      {
+        name: 'Sabrina Nguyen-Hoang',
+        position: 'Co-Director of Design',
+        linkedinURL: 'https://www.linkedin.com/in/sabrina-nguyen-hoang/',
+        imageURL: brie,
+      },
+      {
+        name: 'Alexandria Wang',
+        position: 'Co-Director of Design',
+        linkedinURL: 'https://www.linkedin.com/in/alexandria-wang-44066a187/',
+        imageURL: alexandria,
+      },
+      {
+        name: 'Justine Chou',
+        position: 'Director of Marketing',
+        linkedinURL: 'https://www.linkedin.com/in/justine-chou/',
+        imageURL: justine,
+      },
+      {
+        name: 'Mingjia Wang',
+        position: 'Co-Director of Education',
+        linkedinURL: 'https://www.linkedin.com/in/mingjia-wang/',
+        imageURL: mingjia,
+      },
+      {
+        name: 'David Lu',
+        position: 'Co-Director of Education',
+        linkedinURL: 'https://www.linkedin.com/in/david-lin-yi-lu/',
+        imageURL: david,
+      },
+      {
+        name: 'Matthew Wu',
+        position: 'Treasurer',
+        linkedinURL: 'https://www.linkedin.com/in/matthew-caleb-wu/',
+        imageURL: matthew,
+      },
+      {
+        name: 'Michelle Lin',
+        position: 'Director of Membership',
+        linkedinURL: 'https://www.linkedin.com/in/michellelinCA',
+        imageURL: michellelin,
+      },
+      {
+        name: 'Noah Arsala',
+        position: 'Director of Outreach',
+        linkedinURL: 'https://www.linkedin.com/in/noah-arsala-0324911b3/',
+        imageURL: noah,
+      },
+    ],
+  },
+  npo1: {
+    name: 'NPO 1',
+    members: [
+      {
+        name: 'Hang Cao',
+        position: 'Tech Lead',
+        linkedinURL: 'https://www.linkedin.com/in/hang-m-cao/',
+        imageURL: hang,
+      },
+      {
+        name: 'Justin Liao',
+        position: 'Tech Lead',
+        linkedinURL: 'https://www.linkedin.com/in/justin-liao-1949b3217/',
+        imageURL: justin,
+      },
+      {
+        name: 'Zoya Soy',
+        position: 'Design Lead',
+        linkedinURL: 'https://www.linkedin.com/in/zoyasoy/',
+        imageURL: zoya,
+      },
+    ],
+  },
+  npo2: {
+    name: 'NPO 2',
+    members: [
+      {
+        name: 'Jenny Lee',
+        position: 'Tech Lead',
+        linkedinURL: 'https://www.linkedin.com/in/jenny-lee-6630201b2',
+        imageURL: jenny,
+      },
+      {
+        name: 'Ashley Liu',
+        position: 'Tech Lead',
+        linkedinURL: 'https://www.linkedin.com/in/ashley-liu-6314731b9/',
+        imageURL: ashley,
+      },
+      {
+        name: 'Allison Liu',
+        position: 'Design Lead',
+        linkedinURL: 'https://www.linkedin.com/in/hiallisonliu/',
+        imageURL: allisonliu,
+      },
+    ],
+  },
+  npo3: {
+    name: 'NPO 3',
+    members: [
+      {
+        name: 'Gurneet Cheema',
+        position: 'Tech Lead',
+        linkedinURL: 'https://www.linkedin.com/in/jeff-cheema/',
+        imageURL: gurneetcheema,
+      },
+      {
+        name: 'Madhusudhan Sharma',
+        position: 'Tech Lead',
+        linkedinURL: 'https://www.linkedin.com/in/madhuxsharma/',
+        imageURL: madhusudhan,
+      },
+      {
+        name: 'Carlos Lim',
+        position: 'Design Lead',
+        linkedinURL: 'https://www.linkedin.com/in/carlosalim7/',
+        imageURL: carlos,
+      },
+    ],
+  },
 };
 
 export default members;
