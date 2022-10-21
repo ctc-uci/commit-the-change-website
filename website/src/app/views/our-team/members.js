fb0cf4c2fbb37d6b03c05cb87cd936213916153b
liannejl <liannejl@uci.edu>
2022-10-21 02:35:20 -0700

    added member photo for Lianne

--- website/src/app/views/our-team/members.js
+++ website/src/app/views/our-team/members.js
@@ -31,6 +31,7 @@ import andrewlee from '../../../images/ourTeam/memberImages/andrewlee.jpg';
 import arjunsivakumar from '../../../images/ourTeam/memberImages/arjunsivakumar.jpg';
 import aventchiu from '../../../images/ourTeam/memberImages/aventchiu.jpg';
 import kei from '../../../images/ourTeam/memberImages/kei.jpg';
+import lianne from '../../../images/ourTeam/memberImages/lianne.jpg';
 import evan from '../../../images/ourTeam/memberImages/evan.jpg';
 import michaelpien from '../../../images/ourTeam/memberImages/michaelpien.jpg';
 import carolinenguyen from '../../../images/ourTeam/memberImages/carolinenguyen.jpg';
@@ -282,6 +283,13 @@ const members = {
         position: 'Developer',
         linkedinURL: 'https://www.linkedin.com/in/kei-asakawa/',
         imageURL: kei,
+      },
+
+      {
+        name: 'Lianne Lee',
+        position: 'Developer',
+        linkedinURL: 'https://www.linkedin.com/in/lianne-lee-87605b1ab/',
+        imageURL: lianne,
       },
 
       {
