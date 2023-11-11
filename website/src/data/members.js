57dd834c1cd84cf8d9ba6f14fc6f2ac4d7536d0e
subinqkim <subinlk@uci.edu>
2023-11-11 09:42:29 -0800

    Add member image for Subin Kim

--- website/src/data/members.js
+++ website/src/data/members.js
@@ -33,6 +33,7 @@ import ethanho from '../images/ourTeam/memberImages/ethanho.jpg';
 import matthewc from '../images/ourTeam/memberImages/matthewc.png';
 import stevenzhou from '../images/ourTeam/memberImages/stevenzhou.png';
 import elenakao from '../images/ourTeam/memberImages/elena.jpg';
+import subinkim from '../images/ourTeam/memberImages/subin.jpg';
 // --- ------- ---
 
 // Note for bootcamp task: The members attribute of each section is an ordered list.
@@ -142,6 +143,12 @@ const members = {
         position: 'Developer',
         linkedinURL: 'https://www.linkedin.com/in/h0ethan04/',
         imageURL: ethanho,
+      },
+      {
+        name: 'Subin Kim',
+        position: 'Developer',
+        linkedinURL: 'https://www.linkedin.com/in/subinleekim/',
+        imageURL: subinkim,
       },
       {
         name: 'Maithy Le',
