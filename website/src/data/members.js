6ca915d10b2cdb4869405a406f524cfbe5f73c80
Katy Huang <96160110+KatyH820@users.noreply.github.com>
2024-02-02 00:34:06 -0800

    added profile for katy on ctc website (#445)
    Co-authored-by: Andrew Lee <leeaj8@uci.edu>

--- website/src/data/members.js
+++ website/src/data/members.js
@@ -1,26 +1,26 @@
 // --- Board ---
-import andrewlee from '../images/ourTeam/memberImages/andrewlee.jpg';
-import arjunsivakumar from '../images/ourTeam/memberImages/arjunsivakumar.jpg';
-import carly from '../images/ourTeam/memberImages/carly.jpg';
-import emmyc from '../images/ourTeam/memberImages/emmyc.jpg';
-import gayathri from '../images/ourTeam/memberImages/gayathri.jpeg';
-import justin from '../images/ourTeam/memberImages/justin.jpg';
-import kaila from '../images/ourTeam/memberImages/kailalong.jpeg';
-import kainoa from '../images/ourTeam/memberImages/kainoa.jpeg';
-import maithyle from '../images/ourTeam/memberImages/maithyle.jpg';
-import michellelin from '../images/ourTeam/memberImages/michellelin.jpg';
-import noah from '../images/ourTeam/memberImages/noah.png';
+import andrewlee from "../images/ourTeam/memberImages/andrewlee.jpg";
+import arjunsivakumar from "../images/ourTeam/memberImages/arjunsivakumar.jpg";
+import carly from "../images/ourTeam/memberImages/carly.jpg";
+import emmyc from "../images/ourTeam/memberImages/emmyc.jpg";
+import gayathri from "../images/ourTeam/memberImages/gayathri.jpeg";
+import justin from "../images/ourTeam/memberImages/justin.jpg";
+import kaila from "../images/ourTeam/memberImages/kailalong.jpeg";
+import kainoa from "../images/ourTeam/memberImages/kainoa.jpeg";
+import maithyle from "../images/ourTeam/memberImages/maithyle.jpg";
+import michellelin from "../images/ourTeam/memberImages/michellelin.jpg";
+import noah from "../images/ourTeam/memberImages/noah.png";
 
 // --- Leads ---
-import dang from '../images/ourTeam/memberImages/dang.jpg';
-import joshlipton from '../images/ourTeam/memberImages/joshlipton.jpg';
-import madhu from '../images/ourTeam/memberImages/madhu.jpg';
-import michaelpien from '../images/ourTeam/memberImages/michaelpien.png';
-import michelleblin from '../images/ourTeam/memberImages/michelleblin.jpg';
-import minh from '../images/ourTeam/memberImages/minhnguyen.jpg';
-import ostend from '../images/ourTeam/memberImages/ostend.png';
-import rafael from '../images/ourTeam/memberImages/rafael.jpg';
-import selina from '../images/ourTeam/memberImages/selina.jpg';
+import dang from "../images/ourTeam/memberImages/dang.jpg";
+import joshlipton from "../images/ourTeam/memberImages/joshlipton.jpg";
+import madhu from "../images/ourTeam/memberImages/madhu.jpg";
+import michaelpien from "../images/ourTeam/memberImages/michaelpien.png";
+import michelleblin from "../images/ourTeam/memberImages/michelleblin.jpg";
+import minh from "../images/ourTeam/memberImages/minhnguyen.jpg";
+import ostend from "../images/ourTeam/memberImages/ostend.png";
+import rafael from "../images/ourTeam/memberImages/rafael.jpg";
+import selina from "../images/ourTeam/memberImages/selina.jpg";
 
 // --- Members ---
 import lanaramadan from '../images/ourTeam/memberImages/lanaramadan.png';
@@ -41,6 +41,7 @@ import priskak from '../images/ourTeam/memberImages/priskak.jpg';
 import phillipcutter from '../images/ourTeam/memberImages/phillipcutter.jpg';
 import shawnliu from '../images/ourTeam/memberImages/shawnliu.jpg';
 import chloecheng from '../images/ourTeam/memberImages/chloecheng.jpg';
+import katy from "../images/ourTeam/memberImages/katy.jpg";
 // --- ------- ---
 
 // Note for bootcamp task: The members attribute of each section is an ordered list.
@@ -48,101 +49,101 @@ import chloecheng from '../images/ourTeam/memberImages/chloecheng.jpg';
 // in alphabetical order by first name - last name.
 const members = {
   board: {
-    name: 'Board',
+    name: "Board",
     members: [
       {
-        name: 'Michelle Lin',
-        position: 'President',
-        linkedinURL: 'https://www.linkedin.com/in/michellelinCA',
+        name: "Michelle Lin",
+        position: "President",
+        linkedinURL: "https://www.linkedin.com/in/michellelinCA",
         imageURL: michellelin,
       },
       {
-        name: 'Arjun Sivakumar',
-        position: 'Internal VP',
-        linkedinURL: 'https://www.linkedin.com/in/arjunsvkmr/',
+        name: "Arjun Sivakumar",
+        position: "Internal VP",
+        linkedinURL: "https://www.linkedin.com/in/arjunsvkmr/",
         imageURL: arjunsivakumar,
       },
       {
-        name: 'Noah Arsala',
-        position: 'External VP',
-        linkedinURL: 'https://www.linkedin.com/in/noah-arsala-0324911b3/',
+        name: "Noah Arsala",
+        position: "External VP",
+        linkedinURL: "https://www.linkedin.com/in/noah-arsala-0324911b3/",
         imageURL: noah,
       },
       {
-        name: 'Andrew Lee',
-        position: 'Technical Director',
-        linkedinURL: 'https://www.linkedin.com/in/andrew-lee-0641b380/',
+        name: "Andrew Lee",
+        position: "Technical Director",
+        linkedinURL: "https://www.linkedin.com/in/andrew-lee-0641b380/",
         imageURL: andrewlee,
       },
       {
-        name: 'Justin Liao',
-        position: 'Technical Director',
-        linkedinURL: 'https://www.linkedin.com/in/justin-liao-1949b3217/',
+        name: "Justin Liao",
+        position: "Technical Director",
+        linkedinURL: "https://www.linkedin.com/in/justin-liao-1949b3217/",
         imageURL: justin,
       },
       {
-        name: 'Caroline Guzylak',
-        position: 'Design Director',
-        linkedinURL: 'https://www.linkedin.com/in/cguzylak/',
+        name: "Caroline Guzylak",
+        position: "Design Director",
+        linkedinURL: "https://www.linkedin.com/in/cguzylak/",
         imageURL: carly,
       },
       {
-        name: 'Gayathri Yedavilli',
-        position: 'Marketing Director',
-        linkedinURL: 'https://www.linkedin.com/in/gayathri-yedavilli/',
+        name: "Gayathri Yedavilli",
+        position: "Marketing Director",
+        linkedinURL: "https://www.linkedin.com/in/gayathri-yedavilli/",
         imageURL: gayathri,
       },
       {
-        name: 'Kainoa Nishida',
-        position: 'Treasurer',
-        linkedinURL: 'https://www.linkedin.com/in/kainoa-nishida-308b68253/',
+        name: "Kainoa Nishida",
+        position: "Treasurer",
+        linkedinURL: "https://www.linkedin.com/in/kainoa-nishida-308b68253/",
         imageURL: kainoa,
       },
       {
-        name: 'Kaila Long',
-        position: 'Membership Director',
-        linkedinURL: 'https://www.linkedin.com/in/kaila-long-3952b7210/',
+        name: "Kaila Long",
+        position: "Membership Director",
+        linkedinURL: "https://www.linkedin.com/in/kaila-long-3952b7210/",
         imageURL: kaila,
       },
       {
-        name: 'Maithy Le',
-        position: 'Outreach Director',
-        linkedinURL: 'https://www.linkedin.com/in/maithy-le',
+        name: "Maithy Le",
+        position: "Outreach Director",
+        linkedinURL: "https://www.linkedin.com/in/maithy-le",
         imageURL: maithyle,
       },
       {
-        name: 'Emmy Chen',
-        position: 'Outreach Director',
-        linkedinURL: 'https://www.linkedin.com/in/emmy-chen-583552252',
+        name: "Emmy Chen",
+        position: "Outreach Director",
+        linkedinURL: "https://www.linkedin.com/in/emmy-chen-583552252",
         imageURL: emmyc,
       },
     ],
   },
   aiss: {
-    name: 'Achievement Institute of STEM Scholars',
+    name: "Achievement Institute of STEM Scholars",
     members: [
       {
-        name: 'Michael Pien',
-        position: 'Tech Lead',
-        linkedinURL: 'https://www.linkedin.com/in/michaelpien/',
+        name: "Michael Pien",
+        position: "Tech Lead",
+        linkedinURL: "https://www.linkedin.com/in/michaelpien/",
         imageURL: michaelpien,
       },
       {
-        name: 'Michelle Lin',
-        position: 'Tech Lead',
-        linkedinURL: 'https://www.linkedin.com/in/michellelin1/',
+        name: "Michelle Lin",
+        position: "Tech Lead",
+        linkedinURL: "https://www.linkedin.com/in/michellelin1/",
         imageURL: michelleblin,
       },
       {
-        name: 'Selina Arjomand',
-        position: 'Design Lead',
-        linkedinURL: 'https://www.linkedin.com/in/selinaarjomand/',
+        name: "Selina Arjomand",
+        position: "Design Lead",
+        linkedinURL: "https://www.linkedin.com/in/selinaarjomand/",
         imageURL: selina,
       },
       {
-        name: 'Alyssia Tan',
-        position: 'Developer',
-        linkedinURL: 'https://www.linkedin.com/in/alyssia-tan/',
+        name: "Alyssia Tan",
+        position: "Developer",
+        linkedinURL: "https://www.linkedin.com/in/alyssia-tan/",
         imageURL: alyssia,
       },
       {
@@ -176,50 +177,56 @@ const members = {
         imageURL: maithyle,
       },
       {
-        name: 'Sean Fong',
-        position: 'Developer',
-        linkedinURL: 'https://www.linkedin.com/in/seancfong/',
+        name: "Sean Fong",
+        position: "Developer",
+        linkedinURL: "https://www.linkedin.com/in/seancfong/",
         imageURL: seanfong,
       },
     ],
   },
   s2t: {
-    name: 'Stand Up to Trash',
+    name: "Stand Up to Trash",
     members: [
       {
-        name: 'Ostend Suryajaya',
-        position: 'Tech Lead',
-        linkedinURL: 'https://www.linkedin.com/in/ostend-suryajaya/',
+        name: "Ostend Suryajaya",
+        position: "Tech Lead",
+        linkedinURL: "https://www.linkedin.com/in/ostend-suryajaya/",
         imageURL: ostend,
       },
       {
-        name: 'Dang Nguyen',
-        position: 'Tech Lead',
-        linkedinURL: 'https://www.linkedin.com/in/nwin/',
+        name: "Dang Nguyen",
+        position: "Tech Lead",
+        linkedinURL: "https://www.linkedin.com/in/nwin/",
         imageURL: dang,
       },
       {
-        name: 'Rafael Muñoz',
-        position: 'Design Lead',
-        linkedinURL: 'https://www.linkedin.com/in/rafael-carrillo-mu%C3%B1oz/',
+        name: "Rafael Muñoz",
+        position: "Design Lead",
+        linkedinURL: "https://www.linkedin.com/in/rafael-carrillo-mu%C3%B1oz/",
         imageURL: rafael,
       },
       {
-        name: 'Kevin Wu',
-        position: 'Developer',
-        linkedinURL: 'https://www.linkedin.com/in/kevinwu098/',
+        name: "Katy Huang",
+        position: "Developer",
+        linkedinURL: "https://www.linkedin.com/in/katy-huang-820153223/",
+        imageURL: katy,
+      },
+      {
+        name: "Kevin Wu",
+        position: "Developer",
+        linkedinURL: "https://www.linkedin.com/in/kevinwu098/",
         imageURL: kevinwu,
       },
       {
-        name: 'Matthew Chang',
-        position: 'Developer',
-        linkedinURL: 'http://www.linkedin.com/in/matthew-chang-39754a216',
+        name: "Matthew Chang",
+        position: "Developer",
+        linkedinURL: "http://www.linkedin.com/in/matthew-chang-39754a216",
         imageURL: matthewc,
       },
       {
-        name: 'Nathan Pietrantonio',
-        position: 'Developer',
-        linkedinURL: 'https://www.linkedin.com/in/nathanLP/',
+        name: "Nathan Pietrantonio",
+        position: "Developer",
+        linkedinURL: "https://www.linkedin.com/in/nathanLP/",
         imageURL: nathanpietrantonio,
       },
       {
@@ -243,24 +250,24 @@ const members = {
     ],
   },
   fph: {
-    name: 'Feeding Pets of the Homeless',
+    name: "Feeding Pets of the Homeless",
     members: [
       {
-        name: 'Madhu Sharma',
-        position: 'Tech Lead',
-        linkedinURL: 'https://www.linkedin.com/in/madhuxsharma/',
+        name: "Madhu Sharma",
+        position: "Tech Lead",
+        linkedinURL: "https://www.linkedin.com/in/madhuxsharma/",
         imageURL: madhu,
       },
       {
-        name: 'Josh Lipton',
-        position: 'Tech Lead',
-        linkedinURL: 'https://www.linkedin.com/in/joshua-lipton/',
+        name: "Josh Lipton",
+        position: "Tech Lead",
+        linkedinURL: "https://www.linkedin.com/in/joshua-lipton/",
         imageURL: joshlipton,
       },
       {
-        name: 'Minh Nguyen',
-        position: 'Design Lead',
-        linkedinURL: 'https://www.linkedin.com/in/minhtnguy/',
+        name: "Minh Nguyen",
+        position: "Design Lead",
+        linkedinURL: "https://www.linkedin.com/in/minhtnguy/",
         imageURL: minh,
       },
       {
